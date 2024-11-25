6dcb188461815dc6a0d81ddc3fc93482aa975c62
RahafNagi <jinirets21@gmail.com>
2024-11-25 15:49:23 +0300

    Update 02_homework_01.cpp

--- 18-Programming-4kids/02_homework_01.cpp
+++ 18-Programming-4kids/02_homework_01.cpp
@@ -12,18 +12,29 @@ using namespace std;
 
 int main()
 {
-	cout<<"\n\nGuess the "
+	cout<<"\n\nGuess the "  
 		<<"output\n---\n";
-
+//leave two line spaces and writing Guess The output  then leave two line spaces
+	
 	cout<<10+20-5<<endl;
+	//10+20=30-5=35 then leave 1 line
+	
 	cout<<"14/2"<<"\n";
+	//it will be lift as is ->14/2 then leave line
+	
 	cout<<17-10<<"\n";
+	//17-10=7 then leave line
+	
 	cout<<"endl";
+	//leave another line
+	
 	cout<<"\nPractice makes perfect";
+	//leave line then write ->Practice make perfect
 
 	//cout<<"The way to get started is to quit talking and begin doing";
-
+ //it will not be written into the code because (//)
 	return 0;
 
 	cout<<"\n\nBye\n\n";
+	//will not be wretten ,must write this line above return0
 }
